3174d95ce71575f02d1d99916ac765aafbc2e5dd
Taylor <taylorjeftedasilva@gmail.com>
2021-08-15 14:30:15 -0300

    criando método post e ajustando o get para retornar uma lista de acompanhamentos

--- src/main/java/br/com/eniac/eniac/controller/AcompanhamentoController.java
+++ src/main/java/br/com/eniac/eniac/controller/AcompanhamentoController.java
@@ -1,19 +1,36 @@
 package br.com.eniac.eniac.controller;
 
+import br.com.eniac.eniac.controller.repository.AcompanhamentoRepository;
 import br.com.eniac.eniac.modulos.Acompanhamento;
-import br.com.eniac.eniac.modulos.Enfermidade;
-import br.com.eniac.eniac.modulos.Usuario;
-import br.com.eniac.eniac.modulos.tratamentos.DiabetesObservacoes;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("acompanhamento")
 public class AcompanhamentoController {
 
+    @Autowired
+    private AcompanhamentoRepository acompanhamentoRepository;
+
     @GetMapping
-    public Acompanhamento getAcompanhamento(){
-        return new Acompanhamento(1l, new Usuario(1l, "Anna", "123"), new Enfermidade(new DiabetesObservacoes(false, 201)));
+    public ResponseEntity<List> getAcompanhamento(){
+        List<Acompanhamento> listaAcompanhamento  = acompanhamentoRepository.findAll();
+        return new ResponseEntity(listaAcompanhamento, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Acompanhamento> CriaAcompanhamento(@RequestBody Acompanhamento acomp){
+        try{
+            Acompanhamento acompanhamento = acompanhamentoRepository.save(acomp);
+            return new ResponseEntity(acompanhamento, HttpStatus.CREATED);
+        }catch (Exception ex){
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
     }
 }
