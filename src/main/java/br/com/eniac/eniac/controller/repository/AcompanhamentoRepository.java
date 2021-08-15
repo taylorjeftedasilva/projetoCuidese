9e922b71d211130f60db4b6698de7cf966911828
Taylor <taylorjeftedasilva@gmail.com>
2021-08-15 14:26:26 -0300

    Criando repository

---
+++ src/main/java/br/com/eniac/eniac/controller/repository/AcompanhamentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.eniac.eniac.controller.repository;
+
+import br.com.eniac.eniac.modulos.Acompanhamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcompanhamentoRepository extends JpaRepository<Acompanhamento, Long> {
+}
