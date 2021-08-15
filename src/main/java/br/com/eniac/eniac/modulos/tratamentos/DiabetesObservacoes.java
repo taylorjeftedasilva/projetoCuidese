b2688d105ec2dfe2c24d2ad1138a9b7c2213e4f1
Taylor <taylorjeftedasilva@gmail.com>
2021-08-15 14:33:40 -0300

    removendo atributos finais da classe e lógica de status para uma classe static

--- src/main/java/br/com/eniac/eniac/modulos/tratamentos/DiabetesObservacoes.java
+++ src/main/java/br/com/eniac/eniac/modulos/tratamentos/DiabetesObservacoes.java
@@ -1,56 +1,32 @@
 package br.com.eniac.eniac.modulos.tratamentos;
 
 import br.com.eniac.eniac.modulos.enums.StatusDiabetes;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
+
 @Entity
+@Data
+@NoArgsConstructor
 public class DiabetesObservacoes {
-	public final String NOME_ENFERMIDADE = "DIABETES";
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+	@Value("${NOME_ENFERMIDADE: DIABETES}")
+	public String NOME_ENFERMIDADE = "DIABETES";
 	private boolean jejum;
 	@Enumerated(EnumType.STRING)
 	private StatusDiabetes glicemia;
-	private final int GLICEMIA_JEJUM_NORMAL = 99;
-	private final int GLICEMIA_JEJUM_ALTERADO_INICIAL= 100;
-	private final int GLICEMIA_JEJUM_ALTERADO_FINAL = 125;
-	
-	public DiabetesObservacoes (boolean jejum, int glicemia){
-		this.jejum = jejum;
-		setGlicemia(jejum, glicemia);
-	}
-	
-	public void setJejum(boolean jejum) {
-		this.jejum = jejum;
-	}
-	
-	public boolean getJejum() {
-		return jejum;
-	}
+
 	public String getGlicemia() {
 		String resultadoTratado = String.valueOf(this.glicemia).replace("_", " ");
 		return resultadoTratado;
 	}
-	private void setGlicemia(boolean jejum, int glicemia) {
-			
-			if(jejum && glicemia <= GLICEMIA_JEJUM_NORMAL ) {
-				this.glicemia = StatusDiabetes.GLICEMIA_NORMAL;
-			}
-			else if(jejum && glicemia >= 
-					GLICEMIA_JEJUM_ALTERADO_INICIAL & 
-					glicemia <= GLICEMIA_JEJUM_ALTERADO_FINAL) {
-				this.glicemia = StatusDiabetes.GLICEMIA_ALTERADA;
-			}else if(jejum && glicemia >= 
-					GLICEMIA_JEJUM_ALTERADO_INICIAL) {
-				this.glicemia = StatusDiabetes.DIABETES;
-			}
-			else if(!jejum && glicemia <= 200) {
-				this.glicemia = StatusDiabetes.GLICEMIA_NORMAL;
-			}
-			else if(!jejum && glicemia > 200) {
-				this.glicemia = StatusDiabetes.DIABETES;
-			}
-			
-		}
+
+	public DiabetesObservacoes (boolean jejum, int glicemia){
+		this.jejum = jejum;
+		this.glicemia = LogicaDiabetes.setGlicemia(this.jejum, glicemia);
+	}
 
 }
